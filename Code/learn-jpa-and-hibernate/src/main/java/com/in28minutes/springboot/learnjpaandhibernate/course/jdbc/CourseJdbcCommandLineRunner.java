a35aa9a7aa19ab7056fd638ace175a71264790f4
s-muhammadkamran <s.muhammadkamran@gmail.com>
2024-02-02 21:44:44 +0300

    Using Spring Data JPA finished

--- Code/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ Code/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import com.in28minutes.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
@@ -17,10 +18,14 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     @Autowired
     private CourseJpaRepository jpaRepo;
 
+    @Autowired
+    private CourseSpringDataJpaRepository dataJpaRepo;
+
     @Override
     public void run(String... args) throws Exception {
         jdbcRun(args);
         jpaRun(args);
+        dataJpaRun(args);
     }
 
     private void jdbcRun(String... args) throws Exception {
@@ -77,5 +82,42 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
         System.out.println(jpaRepo.findById(1));
         System.out.println("Printing List of All records using JPA");
         Arrays.stream(jpaRepo.getAllCourses().toArray()).forEach(System.out::println);
+    }
+
+    public void dataJpaRun(String... args) throws Exception {
+        dataJpaRepo.save(
+                new Course(1,"Learn Java Now - Data JPA", "In28Minutes")
+        );
+
+        dataJpaRepo.save(
+                new Course(2,"Learn Spring Now - Data JPA", "In28Minutes")
+        );
+
+        dataJpaRepo.save(
+                new Course(3,"Learn DevOps Now - Data JPA", "In28Minutes")
+        );
+
+        dataJpaRepo.save(
+                new Course(4,"Learn AWS Now - Data JPA", "In28Minutes")
+        );
+
+        /*var course = new Course();
+        course.setId(1);
+        jpaRepo.delete(course);*/
+
+        dataJpaRepo.deleteById(2L);
+
+        System.out.println(dataJpaRepo.findById(1L));
+        System.out.println("Printing List of All records using JPA");
+        Arrays.stream(dataJpaRepo.findAll().toArray()).forEach(System.out::println);
+
+        System.out.println(dataJpaRepo.count());
+
+        System.out.println(dataJpaRepo.findByName("Learn AWS Now - Data JPA"));
+        System.out.println(dataJpaRepo.findByAuthor("In28Minutes"));
+        System.out.println(dataJpaRepo.findByAuthor("In21Minutes"));
+
+
+
     }
 }
--- Code/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
+++ Code/learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -1,4 +1,13 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.springdatajpa;
 
-public class CourseSpringDataJpaRepository {
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+    //Follow the naming convention of your method to use findByColumnName in DB
+    //This way Spring Data JPA will implement it automatically and return
+    public List<Course> findByAuthor(String author);
+    public List<Course> findByName(String name);
 }
