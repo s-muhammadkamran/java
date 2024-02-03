19d0450eda183f5996b01cb8599016fd3c6ce7ca
s-muhammadkamran <s.muhammadkamran@gmail.com>
2024-02-03 21:27:27 +0300

    My first web application

---
+++ Code/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,38 @@
+package com.in28minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+    @RequestMapping(value = {"say-hello", "sayhello"})
+    @ResponseBody
+    public String sayHello(@RequestParam("name") String name) {
+        name = name == null || name.isEmpty() ? "Stranger" : name.trim();
+        return "Hello "+name+"! what are you learning today?";
+    }
+
+    @RequestMapping(value = {"say-hello-html", "sayhellohtml"})
+    @ResponseBody
+    public String sayHelloHtml(@RequestParam("name") String name) {
+        name = name == null || name.isEmpty() ? "Stranger" : name.trim();
+        var res = new StringBuffer();
+        res.append("""
+                <html><head><title>My first Html Page</title></head>
+                <body><h1>Hello &nbsp;""");
+        res.append(name);
+        res.append("!</h1></body></html>");
+        return res.toString();
+    }
+
+    @RequestMapping(value={"say-hello-jsp", "sayhellojsp"})
+    // \src\main\resources\META-INF\resources\WEB-INF\jsp\sayHello.jsp
+    // You can keep the Prefix.Suffix format in application.properties file
+    // prefix=/WEB-INF/jsp
+    // suffix=.jsp
+    public String sayHelloJsp() {
+        return "sayHelloInJsp";
+    }
+}
---
+++ Code/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/sayHelloInJsp.jsp
@@ -0,0 +1,8 @@
+<html>
+  <head>
+    <title>My first HTML Page</title>
+  </head>
+  <body>
+    <h3>My first html page with body in sayHello.jsp</h3>
+  </body>
+</html>
